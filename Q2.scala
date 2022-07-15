17cbe913dad368a8101ec9d9538ad0b3ae2dc6c4
R.A.T.U.Perera <92754215+thirani99@users.noreply.github.com>
2022-07-15 20:43:00 +0530

    Add files via upload

---
+++ Q2.scala
@@ -0,0 +1,10 @@
+object main extends App{
+
+  //output--->
+  def  fahrenheit(t:Double):Double = {
+    return (t* 1.8 + 32)
+  }
+    println(" The temperature is = "+fahrenheit(35) +"  F" )
+
+
+}
